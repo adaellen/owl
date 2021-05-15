edf192938fcf7122a67dd4ed97a336b023414fee
Ada Villa <adaellenvilla@gmail.com>
2021-05-15 16:21:35 -0700

    Set up basic logger functionality. Added the 'log' directory to gitignore file.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use log::{error, info, warn};
+use log4rs;
+
 mod user_interface;
 
 fn main() {
+    log4rs::init_file("src/config/log4rs.yaml", Default::default()).unwrap();
+    info!("booting up");
+    warn!("this is a warning!!!");
+    error!("An error has occurred!!! Grammar included!!!");
+
     println!("Hello, world!");
     user_interface::interface_with_me();
+
+
 }
